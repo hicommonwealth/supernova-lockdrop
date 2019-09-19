5cdff52bcf66e9bcf71aeebd8eda14fdefa0553a
Drew Stone <drewstone329@gmail.com>
2019-09-19 17:59:43 +0300

    Setup scaffold for CLI, finish ETH

---
+++ src/ethLock.js
@@ -0,0 +1,51 @@
+const Web3 = require('web3');
+const { toBN, fromWei } = require('web3').utils;
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const EthereumTx = require('ethereumjs-tx');
+
+const LOCKDROP_JSON = JSON.parse(fs.readFileSync('./eth/build/contracts/Lockdrop.json').toString());
+const LOCKDROP_CONTRACT_ADDRESS = process.env.LOCKDROP_CONTRACT_ADDRESS;
+const COSMOS_ADDRESS = process.env.COSMOS_ADDRESS;
+const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY;
+const INFURA_PATH = process.env.INFURA_PATH;
+const LOCALHOST_URL = 'http://localhost:8545';
+
+function getWeb3(remoteUrl) {
+  let provider;
+  if (ETH_PRIVATE_KEY) {
+    provider = new HDWalletProvider(ETH_PRIVATE_KEY, remoteUrl);
+  } else {
+    provider = new Web3.providers.HttpProvider(remoteUrl);
+  }
+  const web3 = new Web3(provider);
+  return web3;
+}
+
+async function lock(length, amount, comsosAddress, remoteUrl=LOCALHOST_URL) {
+  console.log(`locking ${value} ether into Lockdrop contract for ${length} months. Receiver: ${comsosAddress}`);
+  console.log(`Contract ${LOCKDROP_CONTRACT_ADDRESS}`);
+  const web3 = getWeb3(remoteUrl);
+  const contract = new web3.eth.Contract(LOCKDROP_JSON.abi, lockdropContractAddress);
+  // Grab account's transaction nonce for tx params
+  let txNonce = await web3.eth.getTransactionCount(web3.currentProvider.addresses[0]);
+  // Convert ETH amount submitted into WEI
+  const value = web3.utils.toWei(amount, 'ether');
+  // Create tx params for lock function
+  const tx = new EthereumTx({
+    nonce: txNonce,
+    from: web3.currentProvider.addresses[0],
+    to: lockdropContractAddress,
+    gas: 150000,
+    data: contract.methods.lock(lockLength, comsosAddress, isValidator).encodeABI(),
+    value: toBN(value),
+  });
+  try {
+    // Sign the tx and send it
+    tx.sign(Buffer.from(ETH_PRIVATE_KEY, 'hex'));
+    var raw = '0x' + tx.serialize().toString('hex');
+    const txReceipt = await web3.eth.sendSignedTransaction(raw);
+    console.log(`Transaction hash: ${txReceipt.transactionHash}`);
+  } catch (e) {
+    console.log(e);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,81 @@
-import { connectToLedger, connectToBcoin } from './connect';
+#!/usr/bin/env ts-node
+require('dotenv').config();
+import fs from 'fs';
+import program from 'commander';
+import path from 'path';
+import chalk from 'chalk';
+import { version } from '../package.json';
+import * as atom from './atomLock';
+import * as btc from './btcLock';
+import * as eth from './ethLock';
 
-(async function() {
-  let util = connectToBcoin();
-  let result = await util.lockAndRedeemCLTV(1);
-})();
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+
+const execName = path.basename(process.argv[1]);
+program.version(version)
+  .name(execName)
+  .usage('<protocol> <function> [ARGS...]')
+  .arguments('<protocol> <func> [args...]')
+  .action(async (protocol, func, args) => {
+    if (typeof program.seed === 'undefined' && typeof process.env.SEED === 'undefined') {
+      console.log('');
+      console.log(`\t${error.underline('You must provide a private key seed as a CLI argument or as environment variable!')}`);
+      console.log('');
+      return;
+    }
+
+    const isLock = (func === 'lock');
+    const msg = `${(isLock) ? 'to lock on' : 'to query the lockdrop on'}`;
+
+    // If isLock, then the arguments should be <protocol> lock <length> <amount>
+    if (isLock) {
+      const cmd = (protocol === 'eth' || protocol === 'ethereum' || protocol === 'ETH') ? 'lock-eth'
+        : (protocol === 'btc' || protocol === 'bitcoin' || protocol === 'BTC') ? 'lock-btc'
+          : 'lock-atom';
+      const wrongArgsMsg = `${error.underline('You must provide both length and amount arguments such as ')}${warning.underline(`yarn ${cmd} 10 10 -s <seed>`)}${error.underline('!')}`;
+      const lengthErrorMsg = `${error.underline(`Length "${args[0]}" is not properly formatted, you must submit a number such as `)}${warning.underline(`yarn ${cmd} 10 10 -s <seed>`)}${error.underline('!')}`;
+      const amountErrorMsg = `${error.underline(`Amount "${args[1]}" is not properly formatted, you must submit a number such as `)}${warning.underline(`yarn ${cmd} 10 10 -s <seed>`)}${error.underline('!')}`;
+      assert(args.length === 2, wrongArgsMsg);
+      assert(!Number.isNaN(Number(args[0])), lengthErrorMsg);
+      assert(!Number.isNaN(Number(args[1])), amountErrorMsg);
+    }
+
+    switch (protocol) {
+      case 'eth' | 'ethereum' | 'ETH':
+        console.log(`Using the Supernova Lockdrop CLI ${msg} Ethereum`);
+        break;
+      case 'btc' | 'bitcoin' | 'BTC':
+        console.log(`Using the Supernova Lockdrop CLI ${msg} Bitcoin`);
+        break;
+      case 'atom' | 'cosmos':
+        console.log(`Using the Supernova Lockdrop CLI ${msg} Atom/Cosmos`);
+        break;
+      default:
+        console.log(`Using the Supernova Lockdrop CLI ${msg} Ethereum`);
+        break;
+    }
+  })
+  .option('-s, --seed <hexSeed>', 'A seed to sign transactions with, targetting the desired protocol');
+
+program.on('--help', () => {
+  console.log('');
+  console.log('Examples (TODO):');
+});
+
+program.parse(process.argv);
+
+if (program.args.length === 0) {
+  program.outputHelp();
+  process.exit(1);
+}
+
+function assert(condition, message) {
+    if (!condition) {
+        message = error.underline(message || "Assertion failed");
+        if (typeof Error !== "undefined") {
+            throw new Error(message);
+        }
+        throw message; // Fallback
+    }
+}
